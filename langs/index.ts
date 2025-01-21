86588d02accfcf4b13a300b90ab94d63bbdeae70
Luke Ogburn <logburn@noreply.lukeogburn.com>
2025-01-20 20:58:49 -0500

    add es to index

--- langs/index.ts
+++ langs/index.ts
@@ -1,9 +1,11 @@
 import en from "./en.json";
+import es from "./es.json";
 import zh_cn from "./zh_cn.json";
 import zh_tw from "./zh_tw.json";
 
 export const LANGS = {
   en: en,
+  es: es,
   zh_cn: zh_cn,
   zh_tw: zh_tw,
 };
